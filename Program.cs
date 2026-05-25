604f8b180030bbc437e9ea8e3d8c16f63b8e1d63
Denisse-ds <gojosatoruvss@gmail.com>
2026-05-25 15:31:28 -0600

    Configurar AppDbContext con la cadena de conexión de Azure SQL en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 using FarmaciaSimilares01.Components;
+using FarmaciaSimilares01.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<MedicamentosDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 
 var app = builder.Build();
 
