fb3c8b1d68e9d62b3a6ad25251f579c13f4d0912
Denisse-ds <gojosatoruvss@gmail.com>
2026-05-28 22:39:51 -0600

    Mejora de estilos y comentarios

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -6,4 +6,16 @@
         public string nombre { get; set; } = string.Empty;
         public string Descripcion { get; set; } = string.Empty;
     }
+
+    public class ServicioMedica
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Descripcion { get; set; } = "";
+        public decimal Precio { get; set; }
+        public int Duracion { get; set; }
+        public bool RequiereCita { get; set; }
+        public string Especialidad { get; set; } = "";
+        public int EspecialidadId { get; set; }
+    }
 }
